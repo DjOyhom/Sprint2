ba075e8b2fb55a1a5981cd06fa3c871f852f195f
Rodrigo <=>
2019-10-11 11:26:45 -0300

    Comment Semaphore

--- Semaforo.py
+++ Semaforo.py
@@ -12,32 +12,32 @@ class Hilo(threading.Thread):
         self.id = id
 
     def run(self):
-        semaforo.acquire()
+       # semaforo.acquire()
         print ("Entro el gato al patio")
         sleep(3)
         print("Salio el gato del patio")
-        semaforo.release()
+       # semaforo.release()
     
     def aa(self):
-        semaforo.acquire()
+       # semaforo.acquire()
         print ("Entro el canguro al patio")
         sleep(1)
         print("Salio el canguro del patio")
-        semaforo.release()
+       # semaforo.release()
 
     def aaa(self):
-        semaforo.acquire()
+       # semaforo.acquire()
         print ("Entro el perro al patio")
         sleep(5)
         print("Salio el perro del patio")
-        semaforo.release()
+       # semaforo.release()
 
     def aaaa(self):
-        semaforo.acquire()
+       # semaforo.acquire()
         print ("Entro el pajaro al patio")
         sleep(0.51)
         print("Salio el pajaro del patio")
-        semaforo.release()
+       # semaforo.release()
     
 
 hilos = [Hilo(1), Hilo(2), Hilo(3)]
